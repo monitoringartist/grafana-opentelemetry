114bf1a53109f773c457e0c80f92c7dfa7a5edbc
Jan Garaj <jan.garaj@gmail.com>
2023-09-09 18:40:19 +0100

    Dashboard update

--- grafana/overriderun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# overriding entrypoint, because syslog logging can't be configured in other way
-cp /run.sh /tmp
-sed -i 's/cfg:default.log.mode="console"//g' /tmp/run.sh
-/tmp/run.sh
